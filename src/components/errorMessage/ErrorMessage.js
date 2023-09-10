f427758e2ab4dd38ce8a38ddf7fa1198a5a98229
Marakuya90 <zhilzhil@mail.ru>
2023-09-10 11:42:49 +0300

    error component

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,10 @@
+const ErrorMessage = () => {
+  return (
+    // eslint-disable-next-line
+    <iframe src="https://giphy.com/embed/3ohhwLF7pFnQSvalu8" 
+    width="480" height="480" 
+   allowFullScreen></iframe>
+  )
+}
+
+export default ErrorMessage
